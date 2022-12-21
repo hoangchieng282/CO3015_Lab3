047565a9bf591462215ec8f7bfcb520abbb5042b
chih <hoang.chiengphi@elca.vn>
2022-12-21 23:17:24 +0700

    modified folder name

--- use-case/tc.py
+++ use-case/tc.py
@@ -1,14 +1,14 @@
 data = [{
-    'id': "TC-BEH1-001",
+    'id': "TC-BEH-001",
     'description':'Normal flow',
     'errorMessage':""
 },
 {
-    'id': "TC_BEH2_002",
+    'id': "TC_BEH_002",
     'description':'Not input course name',
     'errorMessage':"- Missing full name"
 },
-{'id': "TC_BEH3_003",
+{'id': "TC_BEH_003",
     'description':'Choose a day in the past',
     'errorMessage':"The course end date must be after the start date."
 }
